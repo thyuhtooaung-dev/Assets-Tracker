4d8b79e483424e14fccab6bdf29da11af2bda9a5
Thyu Htoo Aung <thyuhtoo7@gmail.com>
2026-04-02 10:05:05 +0630

    fix build error

--- crud_nextjs/services/api-client.ts
+++ crud_nextjs/services/api-client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API_BASE_URL =
-    process.env.API_BASE_URL ?? process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
+    process.env.API_BASE_URL ??
+    process.env.NEXT_PUBLIC_API_URL ??
+    "https://assets-tracker-backend.vercel.app";
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
